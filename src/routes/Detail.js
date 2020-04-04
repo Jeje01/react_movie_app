4b23335df7e5a661d3ca8fed4d64418400ab9871
Jeje01 <33060748+Jeje01@users.noreply.github.com>
2020-04-04 21:14:14 +0900

    Added styles on detail pages

---
+++ src/routes/Detail.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Detail.css";
+
+class Detail extends React.Component{
+    componentDidMount(){
+        const { location, history } = this.props;
+        if(location.state === undefined){
+            history.push("/");
+        }
+    }
+    render (){
+        const {location} = this.props;
+        if(location.state){
+            return (
+                <div className="detail__box">
+                    <img src={location.state.poster} alt={location.state.title} title={location.state.title}/>
+                    <div className="detail__container">
+                        <h3 className="detail__title">{location.state.title}</h3>
+                        <h5 className="detail__year">{location.state.year}</h5>
+                        <p class="detail__summary">{location.state.summary}</p>
+                    </div>
+                </div>
+                
+            );
+        } else {
+            return null;
+        }
+    }
+}
+
+export default Detail;
