12d288bc87b686a40b353877e746c7805f8ea006
Jeje01 <33060748+Jeje01@users.noreply.github.com>
2020-04-04 17:01:14 +0900

    Added styles and set navigation of Home and About

--- src/App.js
+++ src/App.js
@@ -1,48 +1,17 @@
-import React from 'react';
-import axios from "axios";
-import Movie from "./Movie";
-//import PropTypes from "prop-types";
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Navigation from "./components/Navigation"
+import Home from "./routes/Home";
 
-class App extends React.Component{
-  state = {
-    isLoading: true,
-    movies: []
-  };
-  getMovies = async () => {
-      const {
-        data: {
-          data : {movies}
-        }
-      } = await axios.get(
-        "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
-        );
-      this.setState({movies, isLoading: false});
-  };
-  componentDidMount(){
-    this.getMovies();
-  }
-  render(){
-    const { isLoading, movies } = this.state;
-    return (
-    <div>
-      {isLoading 
-      ? "Loading..." 
-      : movies.map(movie => {
-        console.log(movie);
-        return (
-          <Movie 
-          key={movie.id}
-          id={movie.id} 
-          year={movie.year} 
-          title={movie.title} 
-          summary={movie.summary} 
-          poster={movie.medium_cover_image}
-          />
-        );
-    })}
-    </div>
-    );
-  }
+function App(){
+  return (
+  <HashRouter>
+    <Navigation />
+    <Route path="/" exact={true} component={Home} />
+    <Route path="/about" component={About}/>
+  </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About(){
+    return <span>about this page: I built it cause i love movies.</span>
+}
+
+export default About;
