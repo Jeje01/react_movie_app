74e45516f86df29c473f5291a81a8a6db3003186
Jeje01 <33060748+Jeje01@users.noreply.github.com>
2020-04-04 15:35:43 +0900

    Updates

---
+++ precache-manifest.747bfc5cf75c795ebcbc2d6e8e73401d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77228e02ecaeff44371cc58f69935dae",
+    "url": "/react_movie_app/index.html"
+  },
+  {
+    "revision": "bd165550181fa7563685",
+    "url": "/react_movie_app/static/css/main.afb4fdf9.chunk.css"
+  },
+  {
+    "revision": "ef4ea2a1ccb44b30bef7",
+    "url": "/react_movie_app/static/js/2.7da8e8a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_movie_app/static/js/2.7da8e8a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd165550181fa7563685",
+    "url": "/react_movie_app/static/js/main.0ff09215.chunk.js"
+  },
+  {
+    "revision": "8d47abbd30b563062fea",
+    "url": "/react_movie_app/static/js/runtime-main.b9f84a0a.js"
+  }
+]);
